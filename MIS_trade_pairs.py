52c25acb81e668f03ca3d9a105078a138c352975
Piyush Kumar <ping.piyushkumar@gmail.com>
2025-06-18 14:52:14 +0530

    Update MIS_trade_pairs.py

--- MIS_trade_pairs.py
+++ MIS_trade_pairs.py
@@ -94,8 +94,8 @@ def main():
                     pnl_pips = total_sell_value - total_buy_value
                     
                     # Capture all buy times and sell times
-                    buy_times = min(pd.to_datetime(t['order_timestamp']) for t in matched_buys)
-                    sell_times = max(pd.to_datetime(t['order_timestamp']) for t in matched_sells)
+                    buy_times = min(pd.to_datetime(t['order_timestamp']) for t in buy_trades)
+                    sell_times = max(pd.to_datetime(t['order_timestamp']) for t in sell_trades)
                     
                     # Identify first and last timestamp across both legs
                     first_leg_time = min(buy_times + sell_times)
